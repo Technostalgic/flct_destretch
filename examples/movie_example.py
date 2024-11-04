aff3e78df7ab45d223184df6d1b667e352dab3f7
Technostalgic <isaiahnsmith@hotmail.com>
2024-11-04 10:46:20 -0700

    added destretching algorithm to example

--- examples/movie_example.py
+++ examples/movie_example.py
@@ -9,6 +9,8 @@ from astropy.io.fits import HDUList, ImageHDU
 # data visualization
 import matplotlib.pyplot as plt
 import matplotlib.animation
+from matplotlib.figure import Figure
+from matplotlib.image import AxesImage
 
 # plot rendering backend
 matplotlib.use("TkAgg")
@@ -37,30 +39,81 @@ test_median_y = np.median(test_data, axis=0)
 ## Visualize Image Data -------------------------------------------------------|
 
 # create a figure and image to render our image data
-figure = plt.figure(figsize=(5,4), dpi=169)
-image = plt.imshow(test_data[0], origin="lower", cmap="copper")
+figure: Figure = plt.figure(figsize=(5,4), dpi=169)
+image: AxesImage = plt.imshow(test_data[0], origin="lower", cmap="copper")
 
-# get handle to the specific subplot to render, and limit its x and y resolution
-subplot = plt.subplot(1, 1, 1)
-subplot.set_xlim(0, 1000)
-subplot.set_ylim(0, 1000)
+# get handle to the specific subplot to render
+plot_pre = plt.subplot(1, 1, 1)
 
-# get handle to subplot title so it can be changed
-subplot_title = subplot.set_title("Before FLCT Destretching")
+# set title and limit its x and y resolution
+plot_pre.set_title("Before FLCT Destretching")
+plot_pre.set_xlim(0, 1000)
+plot_pre.set_ylim(0, 1000)
+
+# hide x and y axis numbers since they are pretty much meaningless for images
+plot_pre.set_xticks([])
+plot_pre.set_yticks([])
 
 # define the function to animate each frame
-def draw_frame(index: int):
+def draw_frame_original(index: int) -> AxesImage:
 	image.set_data(test_data[index])
 	return image
 
 # create an animation to view the image data in succession
-animation = matplotlib.animation.FuncAnimation(
-	figure, draw_frame, frames=11, interval=100
+animation_original = matplotlib.animation.FuncAnimation(
+	figure, draw_frame_original, frames=11, interval=100
 )
 
-# hide x and y axis numbers since they are pretty much meaningless for images
-plt.xticks([])
-plt.yticks([])
-
 # show the visualization
+plt.show()
+
+## Perform Destretching -------------------------------------------------------|
+
+# I think the sizes of each sub-image to relocate via destretch ??
+kernel_sizes: np.array = np.array([32])
+
+# ?? TODO
+scene = np.moveaxis(test_data, 0, -1)
+
+# this goes through each image in the data and applies the destretching 
+# algorithm and stores the result
+result = destretch.reg_loop_series(
+	scene, 
+	test_median_y, 
+	kernel_sizes, 
+	mf=0.08, 
+	use_fft=True 
+)
+
+# deconstruct the results into meaningful typed variables
+answer: np.ndarray[np.float64]
+display: np.ndarray[np.float64]
+r_display: np.ndarray[np.float64]
+destretch_info: destretch.Destretch_params
+(answer, display, r_display, destretch_info) = result
+
+## Visualize new Data ---------------------------------------------------------|
+
+# create the figure and image for rendering the new image data
+figure: Figure = plt.figure(figsize=(5,4), dpi=169)
+image: AxesImage = plt.imshow(answer[:, :, 0], origin="lower", cmap="copper")
+
+# arrange sublot and get handle
+plot_post = plt.subplot(1, 1, 1)
+plot_post.set_title("After FLCT Destretching")
+plot_post.set_xlim(0, 1000)
+plot_post.set_ylim(0, 1000)
+
+def draw_frame_destretched(index: int) -> AxesImage:
+	image.set_data(answer[:, :, index])
+	return image
+
+
+# create a new animationto display the image data resulting from 
+# the destretching
+animation_destretched = matplotlib.animation.FuncAnimation(
+	figure, draw_frame_destretched, frames=11, interval=100
+)
+
+# display the visualization
 plt.show()
