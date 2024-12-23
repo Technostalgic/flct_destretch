ca83dd4e6e73a27e0ac5d4a8d38e130c7ba11b87
Isaiah Smith <isaiahnsmith@hotmail.com>
2024-12-23 02:15:04 -0700

    implement OMargin reference method in testbed

--- flctdestretch/abstraction.py
+++ flctdestretch/abstraction.py
@@ -9,11 +9,13 @@ from astropy.io.fits.hdu import HDUList, ImageHDU, CompImageHDU
 from algorithm import reg_loop, reg_loop_series
 from destretch_params import DestretchParams
 import utility
+import reference_method
 
 def destretch_files(
         filepaths: list[os.PathLike], 
         kernel_sizes: list[int], 
-        index_schema: utility.IndexSchema
+        index_schema: utility.IndexSchema,
+        ref_method: reference_method.RefMethod = None
     ) -> list[
         np.ndarray | DestretchParams
     ]:
@@ -28,6 +30,9 @@ def destretch_files(
         the destretch kernel size, the size of each subframe to be processed
     index_schema : reference_method.IndexSchema
         the index schema to use for the input data files
+    ref_method : reference_method.RefMethod
+        the method to use to apply the reference image for destretching, if not
+        specified, will use reference_method.OMargin
 
     Returns
     -------
@@ -36,6 +41,14 @@ def destretch_files(
     """
 
     time_begin = time.time()
+
+    # set default ref method
+    if ref_method is None:
+        ref_method = reference_method.OMargin.create(
+            filepaths, 
+            index_schema, 
+            5, 5
+        )
 
     # used to enforce the same resolution for all data files
     image_resolution = (-1,-1)
@@ -48,64 +61,56 @@ def destretch_files(
 
     # read each image HDU in each file
     print("Searching for image data in specified files...")
-    for path in filepaths:
+    for i in range(len(filepaths)):
+        path = filepaths[i]
         
-        # convert all data to default index schema
-        image_data: np.ndarray = utility.load_image_data(path)
+        # pass data to reference method so it can do its thing
+        match ref_method:
+            case reference_method.OMargin():
+                ref_method.pass_params(i)
+        
+        # get the image data from ref method if available otherwise load it
+        image_data: np.ndarray = ref_method.get_original_data(i)
+        if image_data is None:
+            image_data = utility.load_image_data(path)
 
-        if len(image_data.shape) == 2:
-
-            # ensure all data matches the first file's resolution
-            if image_resolution[0] < 0:
-                image_resolution = (
-                    image_data.shape[0], 
-                    image_data.shape[1]
-                )
-            elif (
-                image_resolution[0] != image_data.shape[0] or 
-                image_resolution[1] != image_data.shape[1]
-            ): 
-                raise Exception(
-                    f"Resolution mismatch for '{os.fspath(path)}'"
-                )
-
-            # TODO seperate image data by z axis
-            # image_data = np.moveaxis(image_data, 0, -1)[:,:]
-
-            # debug log
-            # print(f"found {hdu_index} in {path}: {image_data.shape}")
-
-            # if no previous image, use self for reference and result 
-            # image, do not process
-            if reference_image is None:
-                reference_image = image_data
-                result_sequence.append(image_data)
-                print("initial image to be used as reference")
-                continue
-
-            image_num = len(result_sequence) + 1
-            print(f"processing image #{image_num}..")
-
-            # perform image destretching
-            result = reg_loop(
-                image_data,
-                reference_image,
-                kernel_sizes,
-                mf=0.08,
-                use_fft=True
+        # ensure all data matches the first file's resolution
+        if image_resolution[0] < 0:
+            image_resolution = (
+                image_data.shape[0], 
+                image_data.shape[1]
+            )
+        elif (
+            image_resolution[0] != image_data.shape[0] or 
+            image_resolution[1] != image_data.shape[1]
+        ): 
+            raise Exception(
+                f"Resolution mismatch for '{os.fspath(path)}'"
             )
 
-            # decompose result
-            answer: np.ndarray
-            destretch_info: DestretchParams
-            answer, _, _, destretch_info = result
+        # debug log
+        # print(f"found {hdu_index} in {path}: {image_data.shape}")
 
-            # save the processed image in the results array
-            result_sequence.append(answer)
+        # get the reference image from the specified reference method
+        reference_image = ref_method.get_reference(i)
 
-            # store previously processed frame as reference image to use
-            # for nex image in series
-            reference_image = result_sequence[-1]
+        # perform image destretching
+        print(f"processing image #{i}..")
+        result = reg_loop(
+            image_data,
+            reference_image,
+            kernel_sizes,
+            mf=0.08,
+            use_fft=True
+        )
+
+        # decompose result
+        answer: np.ndarray
+        destretch_info: DestretchParams
+        answer, _, _, destretch_info = result
+
+        # save the processed image in the results array
+        result_sequence.append(answer)
 
     # output time elapsed
     time_end = time.time()
