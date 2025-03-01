424e1101366304242650b4405e26b3c85de3774b
Isaiah Smith <isaiahnsmith@hotmail.com>
2025-03-01 12:06:18 -0700

    add functionality to write data to video instead of just files, git status

--- flctdestretch/examples/fits_to_mp4.py
+++ flctdestretch/examples/fits_to_mp4.py
@@ -3,21 +3,123 @@ import cv2
 import numpy as np
 from typing import Callable
 
-from astropy.io import fits
 from matplotlib import cm
 
 from utility import IndexSchema, load_image_data
 
+def write_frame(
+    data: np.ndarray, 
+    video_writer: cv2.VideoWriter, 
+    map_rgb: Callable | None, 
+    data_range: float, 
+    data_min: float, 
+    normal_mode: bool = False
+) -> None:
+    
+    #ensure data is 2D
+    if not normal_mode and (data is None or data.ndim != 2):
+        return
+    if normal_mode:
+        if data.ndim != 3:
+            return
+        if data.shape[0] > 3:
+            return
+    
+    # normalize the data to 8-bit range (0-255) for visualization
+    norm_data = np.clip((data - data_min) / data_range, 0, 1)
+    frame: np.ndarray | None = None
+
+    # encode each image dimension as normal map colors if specified
+    if normal_mode:
+
+        # separate rgb channels by data z slice
+        shape = norm_data.shape
+        r = norm_data[0,:,:]
+        g = (
+            np.zeros((shape[-2], shape[-1]))
+                if shape[0] <= 1 else 
+            norm_data[1,:,:] 
+        )
+        b = (
+            np.zeros((shape[-2], shape[-1]))
+                if shape[0] <= 2 else 
+            norm_data[2,:,:] 
+        )
+        
+        # write rgb to frame
+        rgb = np.zeros((3, shape[-2], shape[-1]))
+        rgb[0,:,:] = r
+        rgb[1,:,:] = g
+        rgb[2,:,:] = b
+        frame = (
+            IndexSchema.convert(rgb, IndexSchema.TXY, IndexSchema.XYT) * 
+            255.0
+        ).astype(np.uint8)
+    else:
+        frame = (map_rgb(norm_data) * 255.0).astype(np.uint8)
+    
+    # convert to BGR for video
+    frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
+    # Write the frame to the video
+    video_writer.write(frame_bgr)
+
+def data_to_mp4(
+    datas: list[np.ndarray],
+    out_path: os.PathLike,
+    fps: float = 24.0,
+    color_map: str = "copper",
+    relative_min: float = 0,
+    relative_max: float = 1,
+    normal_mode: bool = False,
+) -> None:
+
+    # grab a test frame to initialize some data
+    test_data = datas[-1]
+
+    # Initialize video writer
+    height, width = test_data.shape[:2]
+    video_writer = cv2.VideoWriter(
+        out_path,
+        cv2.VideoWriter_fourcc(*"mp4v"),
+        fps,
+        (width, height)
+    )
+    frames_written = 0
+
+    # get range of last frame and use it for the rest of the frames
+    data_min = np.min(test_data)
+    data_max = np.max(test_data)
+    data_range = (data_max - data_min)
+    data_min += data_range * relative_min
+    data_max -= data_range * (1 - relative_max)
+    data_range *= (relative_max - relative_min)
+
+    # get the colormap mapping function
+    map = cm.get_cmap(color_map)
+
+    # write each data as a frame in the video
+    for data in datas:
+        write_frame(data, video_writer, map, data_range, data_min, normal_mode)
+        frames_written += 1
+
+    # Release the video writer to save the video
+    if frames_written > 0:
+        video_writer.release()
+        print(f"Video saved to {out_path}, with {frames_written} frames")
+    else:
+        print("No valid frames to create a video.")
+
 def fits_to_mp4(
-        in_dir_path: os.PathLike | list[os.PathLike], 
-        out_path: os.PathLike,
-        fps: float = 24.0,
-        color_map: str = "copper",
-        index_schema: IndexSchema = IndexSchema.TYX,
-        relative_min: float = 0,
-        relative_max: float = 1,
-        normal_mode: bool = False,
-    ):
+    in_files: list[os.PathLike], 
+    out_path: os.PathLike,
+    fps: float = 24.0,
+    color_map: str = "copper",
+    index_schema: IndexSchema = IndexSchema.TYX,
+    relative_min: float = 0,
+    relative_max: float = 1,
+    normal_mode: bool = False,
+) -> None:
     """
     convert a directory of .fits files into an mp4 video
 
@@ -44,125 +146,38 @@ def fits_to_mp4(
         each pixel, if so the frames are rendered as a normal map, using each z
         slice as a seperate R or G or B channel
     """
-    # Get a sorted list of all .fits files in the directory
-    fits_files: list[str]
-    if not hasattr(in_dir_path, "append"):
-        fits_files = sorted([
-            os.path.join(in_dir_path, f)
-            for f in os.listdir(in_dir_path) 
-            if f.lower().endswith('.fits')
-        ])
-    else: fits_files = in_dir_path
 
-    if not fits_files:
-        print("No .fits files found in the directory.")
-        return
+    # grab a test frame to initialize some data
+    test_data = load_image_data(in_files[-1], z_index=None)
 
-    # Initialize variables for video dimensions
-    video_writer = None
+    # Initialize video writer
+    height, width = test_data.shape[:2]
+    video_writer = cv2.VideoWriter(
+        out_path,
+        cv2.VideoWriter_fourcc(*"mp4v"),
+        fps,
+        (width, height)
+    )
     frames_written = 0
 
-    # store data range to normalize data
-    data_min: float | None = None
-    data_max: float | None = None
-    data_range: float | None = None
+    # get range of last frame and use it for the rest of the frames
+    data_min = np.min(test_data)
+    data_max = np.max(test_data)
+    data_range = (data_max - data_min)
+    data_min += data_range * relative_min
+    data_max -= data_range * (1 - relative_max)
+    data_range *= (relative_max - relative_min)
 
     # get the colormap mapping function
     map = cm.get_cmap(color_map)
 
-    for path in fits_files:
+    for path in in_files:
 
         # Open the FITS file and extract image data
         data = load_image_data(path, z_index=None)
         data = IndexSchema.convert(data, index_schema, index_schema.XYT)
-
-        ## TODO fix offsets increasing magnitude along y axis
-        # if pixel_adjust_y:
-        #     print("adjusting for pixel offsets... " + str(frames_written))
-        #     sub = utility.IndexSchema.convert(np.transpose(np.indices((data.shape[0], data.shape[1], data.shape[2]))[0], (2,0,1)), utility.IndexSchema.TYX, utility.IndexSchema.YXT)
-        #     data -= sub
-
-        # ensure data is 2D
-        if not normal_mode and (data is None or data.ndim != 2):
-            print(f"Skipping {path}: Data is not 2D.")
-            continue
-        if normal_mode:
-            if data.ndim != 3:
-                print(
-                    f"Skipping {path}: " +
-                    "normal_mode on, but data has dimensions not equal to 3"
-                )
-                continue
-            if data.shape[0] > 3 :
-                print(
-                    f"Skipping {path}: " +
-                    "Data has to manny z levels."
-                )
-                continue
-
-        # get range of first frame and use it for the rest of the frames
-        if data_min is None:
-            data_min = np.min(data)
-            data_max = np.max(data)
-            data_range = (data_max - data_min)
-            data_min += data_range * relative_min
-            data_max -= data_range * (1 - relative_max)
-            data_range *= (relative_max - relative_min)
         
-        # normalize the data to 8-bit range (0-255) for visualization
-        norm_data = np.clip((data - data_min) / data_range, 0, 1)
-        frame: np.ndarray | None = None
-
-        # encode each image dimension as normal map colors if specified
-        if normal_mode:
-
-            # separate rgb channels by data z slice
-            shape = norm_data.shape
-            r = norm_data[0,:,:]
-            g = (
-                np.zeros((shape[-2], shape[-1]))
-                    if shape[0] <= 1 else 
-                norm_data[1,:,:] 
-            )
-            b = (
-                np.zeros((shape[-2], shape[-1]))
-                    if shape[0] <= 2 else 
-                norm_data[2,:,:] 
-            )
-            
-            # if no b channel, use it to show magnitude
-            # if shape[0] <= 2:
-            #     b = (r ** 2 + b ** 2) ** .5
-            
-            # write rgb to frame
-            rgb = np.zeros((3, shape[-2], shape[-1]))
-            rgb[0,:,:] = r
-            rgb[1,:,:] = g
-            rgb[2,:,:] = b
-            frame = (
-                IndexSchema.convert(rgb, IndexSchema.TXY, IndexSchema.XYT) * 
-                255.0
-            ).astype(np.uint8)
-            # print(data[frames_written * 90, 0, 1])
-
-        # convert to BGR for video
-        else:
-            frame = (map(norm_data) * 255.0).astype(np.uint8)
-        frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-
-        # initialize video writer if not already initialized
-        if video_writer is None:
-            height, width = frame_bgr.shape[:2]
-            video_writer = cv2.VideoWriter(
-                out_path,
-                cv2.VideoWriter_fourcc(*"mp4v"),
-                fps,
-                (width, height)
-            )
-
-        # Write the frame to the video
-        # cv2.imwrite(out_path + f"frame_{frames_written}.png", frame_bgr)
-        video_writer.write(frame_bgr)
+        write_frame(data, video_writer, map, data_range, data_min, normal_mode)
         frames_written += 1
 
     # Release the video writer
